5fe54e8ece7fbf43d29dbcd51c0786d1b96600c0
tofaelkabir <tofaelkabir@gmail.com>
2020-01-13 23:04:02 -0500

    Monday01132020

--- src/main/java/org/mountsinai/qa/test/pages/LearningSeleniumMethods.java
+++ src/main/java/org/mountsinai/qa/test/pages/LearningSeleniumMethods.java
@@ -1,6 +1,8 @@
 package org.mountsinai.qa.test.pages;
 
 import org.mountsinai.qa.test.base.BasePage;
+import org.mountsinai.qa.test.util.ElementUtil;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,17 +10,27 @@ import org.openqa.selenium.support.How;
 
 public class LearningSeleniumMethods extends BasePage {
 	WebDriver driver;
-	//ElementUtil elementUtil;
+	ElementUtil elementUtil;
+	
+	public LearningSeleniumMethods(WebDriver driver) {
+		this.driver = driver;
+		elementUtil = new ElementUtil(driver);
+		elementUtil.waitForElementPresent(logo); //better to use something which is present in home page
 
-	//Number#1 standard
-	@FindBy(xpath = "//img[@alt='Mount Sinai']")
-	public static WebElement logo;
-
-	public void checkLogo() {
-		logo.click();
+	}
+	By logo = By.xpath("//img[@alt='Mount Sinai']");
+	
+	public boolean verifyLogo() {
+		return elementUtil.doIsDisplayed(logo);
 	}
 	
-	//Tanzina please write 5 webelement and method
+	//Number#1 standard
+	@FindBy(xpath = "//h2[starts-with(text(),'Mount Sinai at a Glance')]")
+	public static WebElement mountSinaiAtAGlance;
+
+	public void checkMountSinaiAtAGlance() {
+		mountSinaiAtAGlance.click();
+	}
 	
 	//Number#2
 	//This is other way to write @FindBy
@@ -29,6 +41,39 @@ public class LearningSeleniumMethods extends BasePage {
 	public void checkContactNumber() {
 		contactNumber.click();
 	}
+	
+	@FindBy(xpath = "(//a[contains(text(),'For Physicians')])[1]")
+	WebElement forPhysician;
+	
+	//image, link, button, text field, check box etc.--isDisplayed
+	
+	public boolean verifyForPhysician() {
+		return forPhysician.isDisplayed();
+	}
+	
+	@FindBy(xpath = "(//span[contains(text(),'Specialty')])[3]")
+	WebElement specialityButton;
+	
+	//isSelected() method is predominantly used with radio buttons, dropdowns and checkboxes.
+	
+	public boolean verifySpecialityButton() {
+		return specialityButton.isSelected();
+		}
+	
+	
+	@FindBy(how = How.XPATH, using = "//span[text()='Book an Appointment']")
+	public static WebElement bookAnAppointment;
+	
+	//isEnabled() is primarily used with buttons.
+	
+	public boolean verifyBookAnAppointment() {
+		return bookAnAppointment.isEnabled();
+	}
+	
+	public String verifyHomePageTitle() {
+		return driver.getTitle();
+	}
+	
 	
 	
 	
--- src/test/java/org/mountsinai/qa/tests/LearningSeleniumMethodsTest.java
+++ src/test/java/org/mountsinai/qa/tests/LearningSeleniumMethodsTest.java
@@ -1,16 +1,20 @@
 package org.mountsinai.qa.tests;
 
 import java.util.Properties;
-
 import org.mountsinai.qa.test.base.BasePage;
-import org.mountsinai.qa.test.pages.HomePage;
 import org.mountsinai.qa.test.pages.LearningSeleniumMethods;
+import org.mountsinai.qa.test.util.Constants;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
 
 public class LearningSeleniumMethodsTest {
 	
@@ -18,6 +22,7 @@ public class LearningSeleniumMethodsTest {
 	BasePage basePage;
 	Properties prop;
 	LearningSeleniumMethods lsm;
+	SoftAssert sa;
     
 
 	@BeforeTest
@@ -25,18 +30,85 @@ public class LearningSeleniumMethodsTest {
 		basePage = new BasePage();
 		prop = basePage.init_properties();
 		driver = basePage.init_driver(prop);
+		sa = new SoftAssert();
 		lsm = PageFactory.initElements(driver, LearningSeleniumMethods.class);
 	}
-	//Number#1
-	@Test(priority = 1, enabled = false)
+	
+	@Test()
 	public void testLogo() {
-		lsm.checkLogo();
+		Assert.assertTrue(lsm.verifyLogo(), "Application logo is not present...");
+	}
+	
+	//Number#1
+	@Test(priority = 1, enabled = true)
+	public void testMountSinaiAtAGlance() {
+		lsm.checkMountSinaiAtAGlance();
 	}
 	
 	//Number#2
 	@Test(priority = 2, enabled = true)
 	public void testContactNumber() {
 		lsm.checkContactNumber();
+	}
+	
+	@Test(invocationCount = 2)
+	public void testForPhysician() {
+		lsm.verifyForPhysician();
+		Assert.assertTrue(true, "For Physician WebElement is not present...");
+	}
+	
+	@Test(invocationCount = 2) //RUN 4 TIMES
+	public void testSpecialityButton() {
+		lsm.verifySpecialityButton();
+		Assert.assertTrue(true); //hard assert
+		}
+	@Test
+	public void testBookAnAppointment() {
+		lsm.verifyBookAnAppointment();
+		sa.assertTrue(true); //softassert is used
+		
+		}
+	
+	
+	@Test(dependsOnMethods = {"testLogo"})//THIS TEST DEPEND ON TESTLOGO
+	public void testHomePageTitle() {
+		//String title = driver.getTitle(); // no need to create a method if we use this line
+		String title = lsm.verifyHomePageTitle();
+		System.out.println("home page title is: " + title);
+		Assert.assertEquals(title, Constants.HOME_PAGE_TITLE, "Home Page Title doesn't match...");
+	
+	}
+	
+	
+	//*******************************************************************************//
+	
+	
+	
+	//Number#3 //SHORTEST FORM WITHOUT CREATING METHOD IN ANOTHER PAGE
+	@Test
+	public void testMyChart() {
+		driver.findElement(By.xpath("(//a[contains(text(),'MyChart')])[1]")).click();
+	}
+	
+	//Number#3b //SHORTEST FORM
+	
+	@Test
+	public void testUrgentCareDescription() {
+		driver.findElement(By.xpath("//p[starts-with(text(),'No appointment needed')]")).click();
+		}
+	
+	
+	
+	//Number#4
+	//when locator is ok but still selenium can't find the webelement
+	@Test
+	public void testPayMyBill() {
+		
+		WebElement payMyBill = driver.findElement(By.xpath("(//a[text()='Pay My Bill'])[1]"));
+		
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click()", payMyBill);
+		
 	}
 	
 	@AfterTest
